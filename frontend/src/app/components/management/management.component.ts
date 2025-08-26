984b768b371df8f5e840a685adc8db609364098b
cybercortex <tamir.zehavi01@gmail.com>
2025-08-26 16:23:24 +0300

    Management view UX

--- frontend/src/app/components/management/management.component.ts
+++ frontend/src/app/components/management/management.component.ts
@@ -3,29 +3,31 @@ import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { catchError, EMPTY, tap } from 'rxjs';
 import { UploadService } from '../../services/upload.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-management',
-  imports: [MatIcon],
+  imports: [MatIcon, NgClass],
   templateUrl: './management.component.html',
   styleUrl: './management.component.scss',
 })
 export class ManagementComponent implements OnInit {
   uploadService = inject(UploadService);
   loadingStatus = signal<Common.LoadingStatus>('loading');
+  deleteAllError: string | null = null;
 
   isLoading = computed(() => {
     return this.loadingStatus() === 'loading';
   });
 
-  files = signal<API.FileListResponse>([]);
+  files = signal<FilesModel>([]);
 
   ngOnInit(): void {
     this.uploadService
       .getList()
       .pipe(
         tap((response) => {
-          this.files.set(response);
+          this.mapToModel(response);
           this.loadingStatus.set(Common.LoadingStatus.Success);
         }),
         catchError(() => {
@@ -37,6 +39,7 @@ export class ManagementComponent implements OnInit {
   }
 
   onDeleteFileClick(id: string) {
+    this.updateFile(id, { loading: true, error: false });
     this.uploadService
       .delete(id)
       .pipe(
@@ -44,17 +47,60 @@ export class ManagementComponent implements OnInit {
           this.files.set(this.files().filter((file) => file.id !== id));
         }),
       )
-      .subscribe();
+      .subscribe({
+        error: () => {
+          this.updateFile(id, { error: true, loading: false });
+        },
+      });
   }
 
   onDeleteAllClick() {
+    this.loadingStatus.set(Common.LoadingStatus.Loading);
     this.uploadService
       .deleteAll()
       .pipe(
         tap(() => {
           this.files.set([]);
+          this.loadingStatus.set(Common.LoadingStatus.Pending);
         }),
       )
-      .subscribe();
+      .subscribe({
+        error: () => {
+          this.deleteAllError = 'Failed to delete all files';
+          this.loadingStatus.set(Common.LoadingStatus.Error);
+        },
+      });
+  }
+
+  mapToModel(files: API.FileListResponse) {
+    const model = files.map((file) => {
+      const fileConfig: FileConfig = {
+        name: file.name,
+        id: file.id,
+        error: false,
+        loading: false,
+      };
+      return fileConfig;
+    });
+    this.files.set(model);
+  }
+
+  private updateFile(id: string, patch: Partial<FileConfig>) {
+    this.files.update((files) => {
+      const index = files.findIndex((file) => file.id === id);
+      if (index !== -1) {
+        files[index] = { ...files[index], ...patch };
+      }
+      return files;
+    });
   }
 }
+
+type FilesModel = FileConfig[];
+
+type FileConfig = {
+  id: string;
+  name: string;
+  error: boolean;
+  loading: boolean;
+};
